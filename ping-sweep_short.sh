7f23075cab71e25e488f8dc91c989927d4b9d111
deadtrickz <40606419+deadtrickz@users.noreply.github.com>
2024-10-06 20:52:08 -0500

    :balloon:

---
+++ ping-sweep_short.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Global Variable
+ports=(20 21 22 23 25 43 53 69 79 80 88 110 111 443 445) 
+
+# Script Start
+classIP=$1; startingOctet=$2; lastOctet=$3
+while [ $startingOctet -le $lastOctet ]; do
+    ipAddress=$classIP.$startingOctet
+    ping -c 1 -W 1 $ipAddress > /dev/null && echo "$ipAddress is UP" && for portNumber in ${ports[*]}; do
+        timeout 1 bash -c "echo '' > /dev/tcp/$ipAddress/$portNumber" 2>/dev/null && echo "Port $portNumber is OPEN"
+    done
+    startingOctet=$((startingOctet + 1))
+done
